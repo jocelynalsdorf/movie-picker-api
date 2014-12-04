d2c55dbf2fccab4b694ecd5841c479373468e7fe
jocelynalsdorf <jfrodsla@gmail.com>
2014-12-04 14:24:33 +0700

    converted string from text in put into integer because they want an integer for dates to search

--- apps/main.js
+++ apps/main.js
@@ -4,22 +4,23 @@ $('.user-info').submit(function (event) {
         event.preventDefault();
         // get the value of the tags the user submitted
         var year = $(this).find("input[name='element_1']").val();
-       // nameofmygetFunction(year, fav);
-     
+       	
+        //changes string to integer 
+       	var yearNum = parseInt(year);
+       	//testing will remove-----
+        alert(yearNum + 10);
         var fav = $(this).find("input[name='element-radio']:checked").val();
-       
-
+      
 		if(year === "" || fav === "") {
 		alert("You have not answered Harold's questions yet");
 		}
 		else {
+			//testing will remove and put in a function to start JSON call
 	 	alert(year + fav);
+	 	 //  remember to add nameofmygetFunction(year, fav); so you can run it
 		}
+
 	}); 
-
-
-
-
 
 
 
