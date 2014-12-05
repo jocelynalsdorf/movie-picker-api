9cf66f3c15019d3a1aaa311e3f0135c875771551
jocelynalsdorf <jfrodsla@gmail.com>
2014-12-05 14:32:29 +0700

    got call working for both radio input options, now have correct type of objects returning based on choice user makes, now need to append data to hidden divs

--- apps/pmain.js
+++ apps/pmain.js
@@ -3,7 +3,7 @@ var url = 'http://api.themoviedb.org/3/',
     mode = 'discover/movie?',
     year,
     yearNum,
-    primary_release_year = '2010',
+    fav,
     key = '&api_key=320a3fa0fbd7cfcae7cff15a4d705363';
 
 
@@ -14,7 +14,29 @@ $('.user-info').submit(function (event) {
         
         //changes string to integer 
         var yearNum = parseInt(year);
+        var fav = $(this).find("input[name='element-radio']:checked").val();
+
+if(fav == 1) {
+            
+
         $.ajax({
+            type: 'GET',
+            url: url + mode + "primary_release_year=" +yearNum+ "&vote_average.gte=7.0&vote_count.gte=3&" + key,
+            async: false,
+            jsonpCallback: 'results',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.dir(json);
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+        });
+
+            }
+            else if(fav == 2) {
+                $.ajax({
             type: 'GET',
             url: url + mode + "primary_release_year=" +yearNum+ "&vote_average.lte=4.0&vote_count.gte=3&" + key,
             async: false,
@@ -28,6 +50,7 @@ $('.user-info').submit(function (event) {
                 console.log(e.message);
             }
         });
+            }
 });
 
 });
