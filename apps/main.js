0086746d95a17877f267f45a48018627fc10b835
jocelynalsdorf <jfrodsla@gmail.com>
2014-12-05 11:12:22 +0700

    stuck and can't get call to work, need to get feedback

--- apps/main.js
+++ apps/main.js
@@ -7,20 +7,72 @@ $('.user-info').submit(function (event) {
        	
         //changes string to integer 
        	var yearNum = parseInt(year);
-       	//testing will remove-----
-        alert(yearNum + 10);
-        var fav = $(this).find("input[name='element-radio']:checked").val();
+       	
+
+        //var fav = $(this).find("input[name='element-radio']:checked").val();
       
 		if(year === "" || fav === "") {
 		alert("You have not answered Harold's questions yet");
 		}
 		else {
-			//testing will remove and put in a function to start JSON call
-	 	alert(year + fav);
-	 	 //  remember to add nameofmygetFunction(year, fav); so you can run it
-		}
+			$.getJSON("http://api.themoviedb.org/3/discover/movie?primary_release_year=&vote_average.lte=4.0&vote_count.gte=3&api_key=320a3fa0fbd7cfcae7cff15a4d705363", function(json) {
+		console.log(json);
 
-	}); 
+			});
+                }
+    });
+
+
+
+var showQuestion = function (question) {
+
+    // clone our result template code
+    var resulting = $('.templates .movie-list').clone();
+
+    // Set the question properties in result
+    var titleElem = resulting.find('.movie-title');
+    titleElem.text(question.title);
+
+    // set the date asked property in result
+    var ratings = resulting.find('.average-rating');
+    ratings.text(question.vote_average);
+
+    return resulting;
+};
+
+
+
+/*var getMovieListDog = function () {
+
+	var results = 
+       
+   		 });
+	};
+
+
+     /*   .done(function (results) {
+        $.each(results.items, function (i, item) {
+            var question = showQuestion(item);
+            $('.hidden-holder').append(question);
+        });
+    });
+
+/*var getMovieListCat = function (yearNum) {
+
+var results = $.ajax({
+        url: "http://api.themoviedb.org/3/discover/movie?primary_release_year=" + yearNum + "&vote_average.gte=7.0&vote_count.gte=3&api_key=320a3fa0fbd7cfcae7cff15a4d705363",
+        dataType: "json",
+        type: "GET",
+    });
+}
+
+
+        .done(function (results) {
+        $.each(results.items, function (i, item) {
+            var question = showQuestion(item);
+            $('.hidden-holder').append(question);
+        });
+    });*/
 
 
 
